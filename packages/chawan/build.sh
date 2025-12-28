44e6b40ba4c034d46c4870aa3c2d592c3f8a52f7
xiliuya <xiliuya@aliyun.com>
2025-12-28 14:05:46 +0800

    fix make4

--- packages/chawan/build.sh
+++ packages/chawan/build.sh
@@ -15,7 +15,7 @@ termux_step_pre_configure() {
 	termux_setup_nim
 }
 termux_step_make() {
-	FLAGS=" --cc:clang --gcc.exe=${CC} --gcc.linkerexe=${CC} " make -j $TERMUX_PKG_MAKE_PROCESSES 
+	FLAGS=" --cc:clang --gcc.exe=${CC} --gcc.linkerexe=${CC} " make -j $TERMUX_PKG_MAKE_PROCESSES
 }
 termux_step_install_license() {
 	mkdir -p "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/"
