e2eec221fa3d4fe2da3ae8559fc71f05f93b71c6
xiliuya <xiliuya@aliyun.com>
2025-12-28 13:52:37 +0800

    fix make

--- packages/chawan/build.sh
+++ packages/chawan/build.sh
@@ -14,6 +14,9 @@ TERMUX_PKG_PLATFORM_INDEPENDENT=true
 termux_step_pre_configure() {
 	termux_setup_nim
 }
+termux_step_make() {
+	make $TERMUX_PKG_MAKE_PROCESSES FLAGS=--cc:clang --gcc.exe=${CC} --gcc.linkerexe=${CC}
+}
 termux_step_install_license() {
 	mkdir -p "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/"
 	cp -af "$TERMUX_PKG_SRCDIR/UNLICENSE" "$TERMUX_PREFIX/share/doc/$TERMUX_PKG_NAME/copyright"
