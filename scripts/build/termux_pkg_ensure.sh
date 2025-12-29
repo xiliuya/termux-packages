7d0536a1ecd019fead7a1c7f87392736b76de035
xiliuya <xiliuya@aliyun.com>
2025-12-29 11:55:00 +0800

    add termux_pkg_ensure.sh

---
+++ scripts/build/termux_pkg_ensure.sh
@@ -0,0 +1,21 @@
+termux_pkg_ensure() {
+	local pkg="${1:-unknown}"
+	local pkg_version="${2:-unknown}"
+	local pkg_marker="$TERMUX_BUILT_PACKAGES_DIRECTORY/${pkg}"
+
+	[[ -f "${pkg_marker}" ]] && [[ "$(<"${pkg_marker}")" == "${pkg_version}" ]] && return 0
+
+	case "$TERMUX_APP_PACKAGE_MANAGER" in
+	"apt")
+		dpkg-query -W -f '${db:Status-Status}\n' "${pkg}" 2>/dev/null | grep -q "installed" && return 0
+		;;
+	"pacman")
+		pacman -Q "${pkg}" &>/dev/null && return 0
+		;;
+	*)
+		command -v "${pkg}" &>/dev/null && return 0
+		;;
+	esac
+
+	termux_pkg_error "${pkg}"
+}
